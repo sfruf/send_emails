4d5c3817a3811d5c1e3e6eec1d43fbd9b0219fa2
sfruf <sebastianfruf@gmail.com>
2022-05-17 06:20:14 -0400

    Made a function to load credentials for testing

--- send_email.py
+++ send_email.py
@@ -1,10 +1,17 @@
 import os
+from dotenv import load_dotenv, find_dotenv
 
-from pip import main
+def load_cred():
+    # find .env automagically by walking up directories until it's found
+    dotenv_path = find_dotenv()
+    load_dotenv(dotenv_path)
+    SENDER= os.environ.get("SENDER")
+    PASSWORD = os.environ.get("PASSWORD")
 
+    return SENDER,PASSWORD
+    
 def send_email():
-    print(f'Sender is {SENDER}')
-
+    SENDER,PASSWORD=load_cred()    
     #send some email stuff here
 
 if __name__=='__main__':
--- test_send_email.py
+++ test_send_email.py
@@ -17,15 +17,15 @@ import send_email
 
 
 #check that there is a variable named sender 
-def test_env_load():
-    with pytest.raises(NameError,match='SENDER'):
-        send_email.send_email()
+#def test_env_load():
+#    with pytest.raises(NameError,match='SENDER'):
+#       send_email.send_email()
 #Notes 5/16/22: once I started trying to write the code that would fail this test, I realized that it's a bit silly
 #Essentially you need to write bad code (which uses a variable I know doesn't exist) to get the error
-
+# 5/17/22: Removing it not that this passed since any future version of the code will fail this test 
 
 #Check that after you load SENDER, that it's a gmail.com address
-#def test_sender_gmail():
-#    send_email
-#    assert sender contains 'gmail.com'
+def test_sender_gmail():
+    sender,password=send_email.load_cred()
+    assert 'gmail.com' in sender
 
